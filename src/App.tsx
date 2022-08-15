6d6e5c6096cd65dec066d9a3e631b6f995102343
Anton Pomogaev <antonpomogaev@gmail.com>
2022-08-15 14:34:03 +0300

    feat: setup config files and project structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = (): JSX.Element => (
+  <div className="min-h-screen bg-slate-700">
+    <header className="flex justify-center pt-8 text-emerald-500 font-bold text-4xl">
+      <p>RS Lang</p>
+    </header>
+  </div>
+);
+
+export default App;
