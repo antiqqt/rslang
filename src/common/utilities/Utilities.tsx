80cccb9b7db59497d28913939a6e4d1d02c68187
Kanstantsin <konstantinksh@gmail.com>
2022-08-27 23:01:52 +0300

    feat: add random func from 0 to max

--- src/common/utilities/Utilities.tsx
+++ src/common/utilities/Utilities.tsx
@@ -1,3 +1,5 @@
+/* eslint no-param-reassign: "error" */
+
 export function getRandomPage() {
   const min = 0;
   const max = 29;
@@ -6,6 +8,18 @@ export function getRandomPage() {
 
 export function getRandomWordNumber() {
   const min = 0;
-  const max = 19;
+  const max = 10;
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function getRandom0toMax(max: number) {
+  const min = 0;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export default function shuffle(array: string[]) {
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
