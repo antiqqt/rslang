b22c657ddae257c63169b67ac77ce1d5ff57c95b
Kanstantsin <konstantinksh@gmail.com>
2022-09-03 01:08:56 +0300

    fix: delete unused mock functions

--- src/components/Games/Sprint.tsx
+++ src/components/Games/Sprint.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 import WordData from '../../common/types/WordData'
-import { getFalseWords, getTrueWords } from './CreateCollection';
 import StartGame from './StartGame';
 
 interface Props {
@@ -14,28 +13,9 @@ function Sprint({ preCheckedGroup }: Props): JSX.Element {
   const [group, setGroup] = useState(0);
   const [gameStarted, setGameStarted] = useState(false);
 
-  const trueWords: WordData[] = getTrueWords(20, group);
-  const falseWords: WordData[] = getFalseWords(120, group, trueWords);
-
-  console.log(trueWords, falseWords)
-
   return (
     <article>
       <h2 className="mx-auto text-gray-700 text-4xl p-4 text-center">Спринт</h2>
-      {(preCheckedGroup === undefined) && !gameStarted && <StartGame
-        group={group}
-        setPage={setPage}
-        setGroup={setGroup}
-        isStarted={gameStarted}
-        setIsGameStarted={setGameStarted}
-        gameName='sprint'
-      />}
-      {(gameStarted || preCheckedGroup) &&
-        <span>
-          game started
-          {console.log(group)}
-        </span>
-      }
     </article>
   )
 }
