18cab9ebfc5b5cfdcbc71ef905561ed679950ad6
MikhailPakaliuk <mr.dyurilen@mail.ru>
2022-09-04 23:55:23 +0300

    fix:  styles changed in about item

--- src/components/MainPage/AboutItem.tsx
+++ src/components/MainPage/AboutItem.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
 interface Post {
+  id: number;
   name: string;
   jobTitle: string;
   activity: string;
+  url: string;
 };
 
 interface Props {
   post: Post;
 };
 
-const AboutItem = (props:Props) => {
+function AboutItem(props: Props) {
   return (
-    <section className="flex font-sans">
+    <section className="flex font-sans mb-10 rounded-xl overflow-hidden bg-slate-200 shadow-xl shadow-gray-300">
       <div className="flex-none w-48 relative">
         <img
-          src="/classic-utility-jacket.jpg"
+          src="public/assets/kostya.jpg"
           alt=""
           className="absolute inset-0 w-full h-full object-cover"
           loading="lazy" />
       </div>
-      <form className="flex-auto p-6">
+      <form className="flex-auto p-6 max-w-sm">
         <div className="flex flex-wrap">
           <h2 className="flex-auto text-lg font-semibold text-slate-900">
             {props.post.name}
@@ -29,31 +31,29 @@ const AboutItem = (props:Props) => {
             {props.post.jobTitle}
           </div>
         </div>
-        <div className="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-          <div className="space-x-2 flex text-sm">
-
-          </div>
-        </div>
-        <div className="flex space-x-4 mb-6 text-sm font-medium">
-          <button
-            className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200"
-            type="button"
-            aria-label="Like">
-            <svg width="20" height="20" fill="currentColor" aria-hidden="true">
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
-            </svg>
-          </button>
-        </div>
         <p className="text-sm text-slate-700">
           {props.post.activity}
         </p>
+        <div className="flex space-x-4 mt-3 mb-3 text-sm font-medium">
+          <button
+            className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-300 hover:text-red-300"
+            type="button"
+            aria-label="Like">
+            <a href={props.post.url} >
+              <svg width="20" height="20" fill="currentColor"
+                aria-hidden="true">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+              </svg>
+            </a>
+          </button>
+        </div>
       </form>
     </section>
 
   );
-};
+}
 
 export default AboutItem;
