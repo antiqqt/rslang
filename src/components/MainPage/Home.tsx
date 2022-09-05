fcc554f13b64d9e59cd549c684d07790aa2b509a
MikhailPakaliuk <mr.dyurilen@mail.ru>
2022-09-05 22:01:47 +0300

    fix: editing text paragraf

--- src/components/MainPage/Home.tsx
+++ src/components/MainPage/Home.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 
 import HomeItem from './HomeItem';
@@ -9,28 +8,32 @@ function Home() {
       id: 1,
       title: 'Учебник',
       paragraph: 'Электронный учебник состоит из шести разделов. В каждом разделе 30 страниц по 20 слов. Представлены перевод слова, тематическое изображение, а также произношение как слова отдельно, так и в составе словосочетания.',
+      img:'./assets/img/textbook.jpg',
     },
     {
       id: 2,
       title: 'Словарь',
       paragraph: 'Словарь содержит списки изучаемых слов, слов, которые не нужно учить, а также тех, которые вызывают затруднения. В словаре отражена статистика по каждому разделу и успеваемость учащихся.',
+      img:'./assets/img/dictionary.jpg',
     },
     {
       id: 3,
       title: 'Игры',
-      paragraph: 'Для изучения слов и закрепления запоминания в приложении есть 4 игры: Sprint и Audio Chalenge которые помогут вам в игровой форме «прокачать» словарный запас.',
+      paragraph: 'Для изучения слов и закрепления запоминания в приложении есть 2 игры: Sprint и Audio Chalenge которые помогут вам в игровой форме «прокачать» словарный запас.',
+      img:'./assets/img/gaming.jpg',
     },
     {
       id: 4,
       title: 'Статистика',
       paragraph: 'Весь прогресс обучения можно посмотреть в статистике, где представлены данные как за текущий день, так и за весь период обучения. Информация представлена ​​как в виде таблицы, так и в виде графиков, что очень удобно.',
+      img:'./assets/img/statistics.jpg',
     }
   ]);
 
   return (
     <section>
-      <h1 className="text-center text-4xl" >Преимущества приложения</h1>
-      <p className="text-center text-2xl" >Зарегистрируйтесь, чтобы использовать все возможности</p>
+      <h1 className="pt-3 pb-2 text-center text-4xl" >Преимущества приложения</h1>
+      <p className=" pb-4 text-center text-2xl pb-30" >Зарегистрируйтесь, чтобы использовать все возможности</p>
       <section className="pt-5 pb-5 flex max-w-4xl flex-wrap justify-between items-stretch content-around">
         {cards.map((card) =>
           <HomeItem post={card} key={card.id} />
